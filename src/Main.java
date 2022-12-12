c9ad78aaecdc3a13d554423746837683e420ef15
PCPetkov18 <PCPetkov18@codingburgas.bg>
2022-12-12 13:51:02 +0200

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pharmacy pharmacy1 = new Pharmacy("ASPIRIN", 10, 102, "23/11/2045");
+        Pharmacy pharmacy2 = new Pharmacy("FLUDITEC", 4, 34, "11/01/2034");
+        Pharmacy pharmacy3 = new Pharmacy("BABINI ZUBI", 34, 12, "24/08/2028");
+        ArrayList<Pharmacy> pharmacies = new ArrayList<>();
+        pharmacies.add(pharmacy1);
+        pharmacies.add(pharmacy2);
+        pharmacies.add(pharmacy3);
+        printPharmacyWithLowerPriceThanFive(pharmacies);
+
+    }
+    public static void printPharmacyWithLowerPriceThanFive(ArrayList<Pharmacy> pharmacies) {
+
+        ArrayList<Pharmacy> newPharmacies = new ArrayList<>();
+        for (Pharmacy p : pharmacies) {
+            if(p.price > 5) {
+                newPharmacies.add(p);
+            }
+        }
+
+        for (Pharmacy p : newPharmacies) {
+            System.out.println(p.toString());
+        }
     }
 }
--- src/Pharmacy.java
+++ src/Pharmacy.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Pharmacy {
+public class Pharmacy {
+
+    public String name;
+    public double price;
+    public int quantity;
+    public String expirationDate;
+    public Pharmacy(String name, double price, int quantity, String expirationDate) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Pharmacy - " +
+                "Type:" + name  +
+                " price:" + price +
+                " quantity:" + quantity +
+                " expirationDate: " + expirationDate;
+    }
 }
